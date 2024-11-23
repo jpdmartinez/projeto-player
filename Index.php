9d451a38fc4990e2179693626c239039c4c685a8
João Pedro Martinez <86573573+jpdmartinez@users.noreply.github.com>
2024-11-23 16:48:51 -0300

    Desenvolvimento do index

--- Index.php
+++ Index.php
@@ -7,32 +7,91 @@ require_once('./class/Ataque.php');
 require_once('./class/Defesa.php');
 require_once('./class/Magia.php');
 
+//Itens de Ataque
+
 $item1 = new Ataque('Espada Longa', 7, 'Ataque');
-//echo "{$item1->getName()} - Tamanho: {$item1->getTamanho()} - Classe do item: {$item1->getClasse()} <br>";
-$item2 = new Defesa('Armadura de couro', 4, 'Defesa');
-//echo "{$item2->getName()} - Tamanho: {$item2->getTamanho()} - Classe do item: {$item2->getClasse()} <br>";
-$item3 = new Magia('Grimório', 2, 'Magia');
-//echo "{$item3->getName()} - Tamanho: {$item3->getTamanho()} - Classe do item: {$item3->getClasse()} <br>";
-$item4 = new Ataque('Machado de batalha', 7, 'Ataque');
-$item5 = new Defesa('Escudo', 4, 'Defesa');
+$item2 = new Ataque('Machado de batalha', 7, 'Ataque');
+$item3 = new Ataque('Espada curta', 7, 'Ataque');
+$item4 = new Ataque('Arco e flecha', 7, 'Ataque');
+$item5 = new Ataque('Lança', 7, 'Ataque');
+$item6 = new Ataque('Adaga afiada', 7, 'Ataque');
+$item7 = new Ataque('Clava', 7, 'Ataque');
+$item8 = new Ataque('Machado de guerra', 7, 'Ataque');
+
+
+//Itens de Defesa
+
+$item9 = new Defesa('Armadura de couro', 4, 'Defesa');
+$item10 = new Defesa('Escudo', 4, 'Defesa');
+$item11 = new Defesa('Armadura de ferro', 4, 'Defesa');
+$item12 = new Defesa('Elmo', 4, 'Defesa');
+$item13 = new Defesa('Capa mágica', 4, 'Defesa');
+$item14 = new Defesa('Escudo de madeira', 4, 'Defesa');
+$item15 = new Defesa('Botas resistentes', 4, 'Defesa');
+$item16 = new Defesa('Escudo de ferro', 4, 'Defesa');
+
+
+//Itens de Magia
+
+$item17 = new Magia('Grimório', 2, 'Magia');
+$item18 = new Magia('Poção de mana', 2, 'Magia');
+$item19 = new Magia('Pergaminho de fogo', 2, 'Magia');
+$item20 = new Magia('Elixir de cura', 2, 'Magia');
+$item21 = new Magia('Poção de invisibilidade', 2, 'Magia');
+$item22 = new Magia('Bastão do mago', 2, 'Magia');
+$item23 = new Magia('Orbe de luz', 2, 'Magia');
 $item6 = new Magia('Poção de vida', 2, 'Magia');
 
-$inventario = new Inventario(20);
-$inventario->capacidadeLivre();
+
+//Criação de inventários 
+$inventarioMago = new Inventario(20);
+$inventarioBarbaro = new Inventario(20);
+$inventarioBardo = new Inventario(20);
+$inventarioRanger = new Inventario(20);
+
+//Criação de personagens 
+$mago = new Player("Elrond", 1, $inventarioMago);
+$barbaro = new Player ("Kruk", 1, $inventarioBarbaro);
+$bardo = new Player ("Lyra", 1, $inventarioBardo);
+$ranger = new Player ("Thorne", 1, $inventarioRanger);
 
 
+echo "{$mago->getNickname()}<br>";
+$mago->coletarItem($item17);
+$mago->coletarItem($item18);
+$mago->coletarItem($item23);
+$mago->coletarItem($item13);
+$mago->coletarItem($item6);
+$mago->soltarItem($item23);
+$mago->subirNivel();
+$inventarioMago->capacidadeLivre();
 
-$player1 = new Player ("Teste", 1, $inventario);
-$player1->coletarItem($item1);
-$player1->coletarItem($item2);
-$player1->coletarItem($item3);
-$player1->coletarItem($item4);
-$player1->coletarItem($item5);
-$player1->coletarItem($item6);
-$inventario->capacidadeLivre();
-$player1->soltarItem($item4);
-$inventario->capacidadeLivre();
+echo "<hr>";
 
-$player1->subirNivel();
+echo "{$barbaro->getNickname()}<br>";
+$barbaro->coletarItem($item1);
+$barbaro->coletarItem($item5);
+$barbaro->coletarItem($item7);
+$barbaro->soltarItem($item5);
+$barbaro->coletarItem($item7);
+$inventarioBarbaro->capacidadeLivre();
 
-$inventario->capacidadeLivre();
+
+echo "<hr>";
+
+echo "{$bardo->getNickname()}<br>";
+$bardo->coletarItem($item23);
+$bardo->coletarItem($item13);
+$bardo->coletarItem($item15);
+$bardo->coletarItem($item6);
+$bardo->subirNivel();
+$inventarioBardo->capacidadeLivre();
+$bardo->subirNivel();
+echo "<hr>";
+
+echo "{$ranger->getNickname()}<br>";
+$ranger->coletarItem($item4);
+$ranger->coletarItem($item11);
+$ranger->coletarItem($item3);
+$inventarioRanger->capacidadeLivre();
+echo "<hr>";
